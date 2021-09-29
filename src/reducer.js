a6e4769673a4611c7d0abd31d62304ab996e6639
luisluft <luberluft@gmail.com>
2021-09-29 08:02:00 -0400

    implement remove item feature

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,16 @@
 const reducer = (state, action) => {
-  if (action.type === "CLEAR_CART") {
-    return { ...state, cart: [] };
-  }
+  if (action.type === "CLEAR_CART") return { ...state, cart: [] };
 
+  if (action.type === "REMOVE_ITEM") {
+    const filteredCart = state.cart.filter(
+      (cartItem) => cartItem.id !== action.payload
+    );
+
+    return {
+      ...state,
+      cart: filteredCart,
+    };
+  }
   return state;
 };
 
